2a1b77c510a3a0c9b85b657498a6aa37c57a39d7
KT-250r <c011921705@edu.teu.ac.jp>
2022-04-07 02:33:04 -0700

    Initial Commit

---
+++ connect_db.php
@@ -0,0 +1,31 @@
+<?php
+	$dsn = "sqlite:/var/www/html/php/login.db"; //データベースの場所指定
+	$id = 1;
+
+	try {
+		$dbh = new PDO($dsn); //オブジェクト作成
+		$sql = "select * from people where member_id = :id"; //prepareメソッドの場合、SQLの一部を引数で変更することができる
+		$stmt = $dbh -> prepare($sql); //SQL文の準備
+		$stmt -> bindParam(":id", $id); //パラメータを固定
+		$stmt -> execute(); //SQL文実行、stmtに結果を格納
+		$rec = $stmt -> fetchAll();  //結果の処理
+	}catch(PDOException $e){ //データベースと接続失敗した場合、PDOExceptionという例外を投げる
+		exit(mb_convert_encoding($e->getMessage(), "UTF-8", "SJIS-win"));
+		die(); //必要か分からん
+	}
+
+	// function printItem($str){
+	// 	return htmlspecialchars($str, ENT_QUOTES, "utf-8");
+	// }
+?>
+
+<hmtl lang="ja">
+	<head>
+		<title>test</title>
+	</head>
+	<body>
+		<?php foreach ($rec as list($id_item, $name_item, $pass_item)): ?>
+			<?php echo $id_item . "<br>" . $name_item . "<br>" . $pass_item; ?>
+		<?php endforeach; ?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>ログインフォーム</title>
+	</head>
+	<body>
+		こんにちは、<?php echo htmlspecialchars($_POST["name"]); ?>さん。
+		<br>
+		あなたは、<?php echo (int)$_POST["age"]; ?>歳です。	
+	</body>
+</html>
